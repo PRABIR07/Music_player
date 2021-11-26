65f5fab8fbc3340d7ed5c4e7ecf8c8d3701d0ae2
PRABIR07 <uic.19mca1007@gmail.com>
2021-11-26 11:32:51 +0530

    first commit

---
+++ music-list.js
@@ -0,0 +1,36 @@
+
+
+let allMusic = [
+    {
+      name: "battleo",
+      artist: "Jordan Schor",
+      img:"cinematic-fairy",
+      src: "cinematic-fairy"
+    },
+    {
+      name: "Ikson Anywhere – Ikson",
+      artist: "Audio Library",
+      img: "battleo",
+      src: "battleo"
+    },
+    {
+      name: "Beauz & Jvna - Crazy",
+      artist: "Beauz & Jvna",
+      img: "cinematic",
+      src: "cinematic"
+    },
+    {
+      name: "Hardwind - Want Me",
+      artist: "Mike Archangelo",
+      img: "emotional-piano",
+      src: "emotional-piano"
+    },
+    {
+      name: "Jim - Sun Goes Down",
+      artist: "Jim Yosef x Roy",
+      img: "cinematic",
+      src: "cinematic"
+    },
+    
+   
+  ];
